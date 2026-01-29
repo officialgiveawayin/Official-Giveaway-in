2049e9a630b3095b34dc81c6e85640e5bf20c9df
officialgiveawayin <blackhunter.125r@gmail.com>
2026-01-29 08:00:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,26 @@ setInterval(() => {
   document.getElementById("countdown").innerText =
     `Draw ends in: ${d}d ${h}h ${m}m ${s}s`;
 }, 1000);
+// About Us Modal Script
+var modal = document.getElementById("aboutModal");
+var btn = document.getElementById("aboutBtn");
+var span = document.getElementsByClassName("close")[0];
+
+if(btn) {
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+}
+
+if(span) {
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+                            }
+                        
